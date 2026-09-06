2e8dda993eb07e68928e535c1b0211a1113317b8
Jaime Sastre Crespo <112887860+JaimeSastreCrespo@users.noreply.github.com>
2026-09-06 20:19:24 +0200

    Update streamlit_app.py
    response needs to be json

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,6 @@ if ingredients_list:
 # New section to display smoothiefroot nutriation information
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
